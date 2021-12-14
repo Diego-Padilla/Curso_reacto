00e747a8acf1fcb52f84996aad8bd1d954517968
Diego Padilla <diego.padilla.dev@gmail.com>
2021-12-13 19:37:20 -0600

    Learning React Context, adding custom hook for shopping cart state

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -12,25 +12,28 @@ import Checkout from '@pages/Checkout';
 import Orders from '@pages/Orders';
 import NotFound from '@pages/NotFound';
 import '@styles/global.css';
+import AppContext from "../context/AppContext"
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Layout>
-				<Routes>
-					<Route path="/"  element ={<Home/>} />
-					<Route path="/login" element={<Login/>} />
-					<Route path="/password-recovery" element={<PasswordRecovery/>} />
-					<Route path="/send-email" element={<SendEmail/>} />
-					<Route path="/new-password" element={<NewPassword/>} />
-					<Route path="/account" element={<Account/>} />
-					<Route path="/signup" element={<CreateAccount/>} />
-					<Route path="/checkout" element={<Checkout/>} />
-					<Route path="/orders" element={<Orders/>} />
-					<Route path="*" element={<NotFound/>} />
-				</Routes>
-			</Layout>
-		</BrowserRouter>
+		<AppContext.Provider value = {} >
+			<BrowserRouter>
+				<Layout>
+					<Routes>
+						<Route path="/"  element ={<Home/>} />
+						<Route path="/login" element={<Login/>} />
+						<Route path="/password-recovery" element={<PasswordRecovery/>} />
+						<Route path="/send-email" element={<SendEmail/>} />
+						<Route path="/new-password" element={<NewPassword/>} />
+						<Route path="/account" element={<Account/>} />
+						<Route path="/signup" element={<CreateAccount/>} />
+						<Route path="/checkout" element={<Checkout/>} />
+						<Route path="/orders" element={<Orders/>} />
+						<Route path="*" element={<NotFound/>} />
+					</Routes>
+				</Layout>
+			</BrowserRouter>
+		</AppContext.Provider>
 	);
 }
 
